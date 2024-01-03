63685ce0674d55eeea66b77203de19ea15542832
Földi Kriszti <fkrt2001@gmail.com>
2024-01-03 18:25:33 +0100

    Finomítgatások
    Egyéb apróbb esztétikai hibák javítása

--- Weboldal/admin.php
+++ Weboldal/admin.php
@@ -7,13 +7,12 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kakukktojás megkeresése</title>
+    <title>Kakukktojás keresés</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="styles/mainstyle.css">
 </head>
 <body>
-
     <?php
     require_once "connect.php";
     require_once "crud/Read.php";
@@ -26,12 +25,12 @@ session_start();
     }
     ?>
     
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="login.php">Főoldal</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-      </button>
+	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="login.php">Cicák!</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
       <ul class="navbar-nav">
         <li class="nav-item dropdown">
@@ -41,38 +40,40 @@ session_start();
           <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
 			  <li><a class="dropdown-item" href="problema.php">Probléma jelentés</a></li>
 			  
-      <?php
+            <?php
       if(!isset($_SESSION["id"])){
-        echo '<a class="dropdown-item" href="login.php">Belépés</a>
-              <a class="dropdown-item" href="register.php">Regisztráció</a>';
+        echo '
+			<a class="dropdown-item" href="login.php">Belépés</a>
+			<a class="dropdown-item" href="register.php">Regisztráció</a>
+			';
+        }else{
+          if(isAdmin($conn,$_SESSION["user"])){
+            echo '
+            <li class="dropdown-item">
+            <h6>Cicás dolgok:</h6>
+          <a class="dropdown-item" href="kakukk.php">Kakukktojás keresés</a>
+          <a class="dropdown-item" href="admin.php">Admin felület</a>
+            <li class="dropdown-item">
+            <h6>Profil:</h6>
+              <a class="dropdown-item" href="passwordChange.php">Jelszóváltás</a>
+              <a class="dropdown-item" href="emailChange.php">Emailváltás</a>
+              <a class="dropdown-item" href="logout.php">Kijelentkezés<i class="glyphicon glyphicon-log-out"></i></a>
+          </li>
+            <script>console.log("    admin   ")</script>';
           }else{
-            if(isAdmin($conn,$_SESSION["user"])){
-              echo '
-              <li class="dropdown-item">
-              <h6>Játékok</h6>
-            <a class="dropdown-item" href="kakukk.php">Kakukktojás megkeresése</a>
-            <a class="dropdown-item" href="admin.php">Admin felület</a>
-              <li class="dropdown-item">
-              <h6>Műveletek:</h6>
-                <a class="dropdown-item" href="passwordChange.php">Jelszó megváltoztatása</a>
-                <a class="dropdown-item" href="emailChange.php">E-mail megváltoztatása</a>
-                <a class="dropdown-item" href="logout.php">Kijelentkezés<i class="glyphicon glyphicon-log-out"></i></a>
-            </li>
-              <script>console.log("    admin   ")</script>';
-            }else{
-              
-              echo '
-          <li class="dropdown-item">
-          <h6>Játékok:</h6>
-        <a class="dropdown-item" href="kakukk.php">Kakukktojás megkeresése</a>
-          <li class="dropdown-item">
-          <h6>Műveletek:</h6>
-            <a class="dropdown-item" href="passwordChange.php">Jelszó megváltoztatása</a>
-            <a class="dropdown-item" href="emailChange.php">E-mail megváltoztatása</a>
-            <a class="dropdown-item" href="logout.php">Kijelentkezés<i class="glyphicon glyphicon-log-out"></i></a>
-        </li>
-          ';
-        }
+            
+            echo '
+        <li class="dropdown-item">
+        <h6>Cicás dolgok:</h6>
+		  <a class="dropdown-item" href="kakukk.php">Kakukktojás keresés</a>
+        <li class="dropdown-item">
+        <h6>Profil:</h6>
+          <a class="dropdown-item" href="passwordChange.php">Jelszóváltás</a>
+          <a class="dropdown-item" href="emailChange.php">Emailváltás</a>
+          <a class="dropdown-item" href="logout.php">Kijelentkezés<i class="glyphicon glyphicon-log-out"></i></a>
+      </li>
+        ';
+      }
           
 			
       }
